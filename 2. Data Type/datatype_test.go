43cdee179eab639c88afa11f61ece4b5d5431ed9
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-03 09:55:18 +0700

    Add More Test Scenario and Refactor Assertion into New Function

--- 2. Data Type/datatype_test.go
+++ 2. Data Type/datatype_test.go
@@ -3,36 +3,78 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	sum := Add(2, 3)
-	expected := 2 + 3
+	t.Run("Simple Adding Test", func(t *testing.T) {
+		sum := Add(2, 3)
+		expected := 2 + 3
 
-	if sum != expected {
-		t.Errorf("sum %d expected %d", sum, expected)
-	}
+		assertCorrectIntValue(t, sum, expected)
+	})
+
+	t.Run("More Adding Test", func(t *testing.T) {
+		sum := Add(4124, 9475)
+		expected := 4124 + 9475
+
+		assertCorrectIntValue(t, sum, expected)
+	})
 }
 
 func TestReduction(t *testing.T) {
-	sum := Reduction(211, 74)
-	expected := 211 - 74
+	t.Run("Simple Reduction Test", func(t *testing.T) {
+		sum := Reduction(211, 74)
+		expected := 211 - 74
 
+		assertCorrectIntValue(t, sum, expected)
+	})
+
+	t.Run("More Reduction Test", func(t *testing.T) {
+		sum := Reduction(92345, 3642)
+		expected := 92345 - 3642
+
+		assertCorrectIntValue(t, sum, expected)
+	})
+}
+
+func TestMultiply(t *testing.T) {
+	t.Run("Simple Multiply Test", func(t *testing.T) {
+		sum := Multiply(32.0, 21.0)
+		expected := 32.0 * 21.0
+
+		assertCorrectFloatValue(t, sum, expected)
+	})
+
+	t.Run("More Multiply Test", func(t *testing.T) {
+		sum := Multiply(94.8, 74)
+		expected := 94.8 * 74
+
+		assertCorrectFloatValue(t, sum, expected)
+	})
+}
+
+func TestDivider(t *testing.T) {
+	t.Run("Simple Divider Test", func(t *testing.T) {
+		sum := Divider(318, 24.0)
+		expected := 318 / 24.0
+
+		assertCorrectFloatValue(t, sum, expected)
+	})
+
+	t.Run("More Divider Test", func(t *testing.T) {
+		sum := Divider(9026, 21.5)
+		expected := 9026 / 21.5
+
+		assertCorrectFloatValue(t, sum, expected)
+	})
+}
+
+func assertCorrectIntValue(t *testing.T, sum, expected int) {
+	t.Helper()
 	if sum != expected {
 		t.Errorf("sum %d expected %d", sum, expected)
 	}
 }
 
-func TestMultiply(t *testing.T) {
-	sum := Multiply(32.0, 21.0)
-	expected := 32.0 * 21.0
-
-	if sum != expected {
-		t.Errorf("sum %f expected %f", sum, expected)
-	}
-}
-
-func TestDivider(t *testing.T) {
-	sum := Divider(318, 24.0)
-	expected := 318 / 24.0
-
+func assertCorrectFloatValue(t *testing.T, sum float64, expected float64) {
+	t.Helper()
 	if sum != expected {
 		t.Errorf("sum %f expected %f", sum, expected)
 	}
