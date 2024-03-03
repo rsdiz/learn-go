d276bcf33df0be440d207ee975c923d8333d28e6
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-03 10:32:32 +0700

    Lesson 4: Learn Constant

---
+++ 4. Constant/constant.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// Constant is a variable whose value cannot be changed again
+	// after it is first assigned a value
+
+	// To create constant, use keyword "const",
+	// and we must immediately initialize with data
+
+	const name = "Muhammad Rosyid Izzulkhaq"
+
+	// Like in variable, we can declare multiple constant
+
+	const (
+		currentYear  uint16 = 2024
+		currentMonth uint8  = 3
+		currentDate  uint8  = 2
+	)
+}
