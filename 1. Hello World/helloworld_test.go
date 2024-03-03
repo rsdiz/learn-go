a9434da6baedee788e02ae1d68a25b6626772573
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-03 09:15:13 +0700

    Refactor Assertion Into A New Function

--- 1. Hello World/helloworld_test.go
+++ 1. Hello World/helloworld_test.go
@@ -3,9 +3,15 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello World!"
+	t.Run("Saying Hello World", func(t *testing.T) {
+		got := Hello()
+		want := "Hello World!"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
