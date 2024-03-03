5e046810a001b7801ea8d573a7104e601d633ec9
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-03 14:35:33 +0700

    Lesson 6: Learn Type Declarations

---
+++ 6. Type Declarations/typedeclaration.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Type declarations is ability to recreate new data types from existing data types
+	// Type declarations usually used to create alias
+	// To create type declaration, use keyword:
+	// type typeName dataType
+
+	type Cuaca string
+
+	var overcast Cuaca = "Mendung"
+	rain := Cuaca("Hujan")
+
+	fmt.Println("Suasana saat ini", overcast)
+	fmt.Println("Kemungkinan nanti akan", rain)
+}
