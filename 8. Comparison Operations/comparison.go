88830d104bd20972a67b950f44e6bb979cea122b
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-04 09:45:22 +0700

    Lesson 8: Learn Comparison Operations

---
+++ 8. Comparison Operations/comparison.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Comparison operation is operation for compare two data
+	// the operation will produce a boolean value
+	// operator:
+	// > (more than)
+	// < (less than)
+	// >= (more than equal)
+	// <= (less than equal)
+	// == (equal)
+	// != (not equal to)
+
+	value1 := 100
+	value2 := 150
+
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 >= value2)
+	fmt.Println(value1 <= value2)
+}
