f692d3fe13a1107238f459f98c5af2fd5e60a3d5
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-05 11:00:57 +0700

    Lesson 13: Learn Function

---
+++ 13. Function/function.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func PrintHello() {
+	fmt.Println("Hello")
+}
+
+func main() {
+	// Function
+	// is a block of code intentionally created in a program so that can be used repeatedly
+	// to create function, we need keyword "func" following by the function name, and the code block
+
+	PrintHello()
+}
