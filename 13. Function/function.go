e49aa19992e64f714a5e31394f4098d8351c95ca
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-05 11:47:28 +0700

    Learn Function with Parameter

--- 13. Function/function.go
+++ 13. Function/function.go
@@ -6,10 +6,22 @@ func PrintHello() {
 	fmt.Println("Hello")
 }
 
+func PrintHelloTo(name string) {
+	fmt.Println("Hello", name)
+}
+
 func main() {
 	// Function
 	// is a block of code intentionally created in a program so that can be used repeatedly
 	// to create function, we need keyword "func" following by the function name, and the code block
 
 	PrintHello()
+
+	// Parameter Function
+	// When we create function, sometimes we need data from outside, or what we call parameters
+	// We can input parameter more than one
+	// Parameter is not required, so we can create function without parameter
+
+	name := "Rosyid"
+	PrintHelloTo(name)
 }
