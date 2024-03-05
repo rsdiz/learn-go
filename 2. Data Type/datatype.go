83793235a591b67dd8d2944a73fd4a53a8c7dd15
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-05 09:48:40 +0700

    Create Function to Update Dictionary Map

--- 2. Data Type/datatype.go
+++ 2. Data Type/datatype.go
@@ -73,6 +73,10 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
+func (d Dictionary) Update(word, definition string) {
+	d[word] = definition
+}
+
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
