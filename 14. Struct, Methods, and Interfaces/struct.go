9c9c82d1f10cb4dd4c1ef91937d595b87ba2994d
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-07 08:51:49 +0700

    Lesson 14: Learn Struct

---
+++ 14. Struct, Methods, and Interfaces/struct.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/**
+Struct is a data template used to combine zero or more other data types into one unit.
+Struct usually the representation of data in the programs we create.
+Every data in structs are saved in the field.
+We can call struct is collection of fields.
+*/
+
+type User struct {
+	fullName    string
+	dateOfBirth time.Time
+	phoneNumber string
+}
+
+func main() {
+	// First way to initialize struct
+	location, _ := time.LoadLocation("Asia/Jakarta")
+	user := User{
+		fullName:    "Rosyid",
+		dateOfBirth: time.Date(2000, 5, 24, 0, 0, 0, 0, location),
+		phoneNumber: "08123456789",
+	}
+	fmt.Printf("Name: %s\nDate of Birth: %s\nPhone Number: %s\n", user.fullName, user.dateOfBirth.Format("Monday, 2 January 2006"), user.phoneNumber)
+
+	// Second way to initialize struct
+	user2 := User{}
+	user2.fullName = "Izzulkhaq"
+	user2.dateOfBirth = time.Date(2008, 7, 1, 0, 0, 0, 0, location)
+	user2.phoneNumber = "08987654321"
+
+	fmt.Printf("Name: %s\nDate of Birth: %s\nPhone Number: %s\n", user2.fullName, user2.dateOfBirth.Format("Monday, 2 January 2006"), user2.phoneNumber)
+}
