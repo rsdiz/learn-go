93b8c3febb31c1b64e4e676e477de56c95315e89
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-08 09:31:07 +0700

    Refactor and Add Unit Test for Area Method

--- 14. Struct, Methods, and Interfaces/struct_test.go
+++ 14. Struct, Methods, and Interfaces/struct_test.go
@@ -7,17 +7,31 @@ func TestPerimeter(t *testing.T) {
 	got := Perimeter(rectangle)
 	want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	assertCorrectFloatValue(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		assertCorrectFloatValue(t, got, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		assertCorrectFloatValue(t, got, want)
+	})
+}
+
+func assertCorrectFloatValue(t testing.TB, got, want float64) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %f want %f", got, want)
+		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
