645c99801fd3d6609d2732560d2e696214d81979
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-08 10:06:13 +0700

    Refactor Unit Test Duplicates for Test Perimeter Method

--- 14. Struct, Methods, and Interfaces/struct_test.go
+++ 14. Struct, Methods, and Interfaces/struct_test.go
@@ -5,20 +5,20 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
+	checkPerimeter := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Perimeter()
+		assertCorrectFloatValue(t, got, want)
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Perimeter()
-		want := 40.0
-
-		assertCorrectFloatValue(t, got, want)
+		checkPerimeter(t, rectangle, 40.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Perimeter()
-		want := 62.83185307179586
-
-		assertCorrectFloatValue(t, got, want)
+		checkPerimeter(t, circle, 62.83185307179586)
 	})
 }
 
