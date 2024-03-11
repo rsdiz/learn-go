d312cd50c6c435f51d5d933603236da97fb3f7d2
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-11 18:17:24 +0700

    Lesson 16: Learn GOPATH

---
+++ 16. GOPATH/gopath.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+GOPATH
+is an environment variable that contains the location where we store Go-Lang projects and libraries.
+GOPATH is important to create when we start the project with many files or need to use library from outside.
+
+to set GOPATH,
+- edit .bashrc or .zshrc (mac or linux) / edit environment variable (windows)
+- add this: (directory can be changed to what we want)
+	export GOPATH="/Users/name/Development/GOLANG"
+- create 'src' directory in that path, and move your current project (or create new project) on it.
+*/
