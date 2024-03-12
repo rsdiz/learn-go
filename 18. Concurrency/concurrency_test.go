244f0be2b39c4f0d6f18723b09fff1fe7529a9f1
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-12 17:49:53 +0700

    Create Benchmark Test for Check Website Function

--- 18. Concurrency/concurrency_test.go
+++ 18. Concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -29,5 +30,21 @@ func TestCheckWebsite(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsite(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
