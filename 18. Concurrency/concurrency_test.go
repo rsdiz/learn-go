c1340ef8537ee03b2e40cf68926b3d5e01aaed22
Muhammad Rosyid Izzulkhaq <rosyid@genilogi.id>
2024-03-15 10:23:41 +0700

    Learn About Race Condition

--- 18. Concurrency/concurrency_test.go
+++ 18. Concurrency/concurrency_test.go
@@ -13,6 +13,15 @@ func mockWebsiteChecker(url string) bool {
 	return true
 }
 
+// If you're lucky, the test will pass, and sometimes test will fail
+// Sometimes, when we run our tests, two of the goroutines write to the results map at exactly the same time.
+// Maps in Go don't like it when more than one thing tries to write to them at once, and so fatal error.
+// This is a race condition, a bug that occurs when the output of our software is dependent on
+// the timing and sequence of events that we have no control over.
+// Because we cannot control exactly when each goroutine writes to the results map,
+// we are vulnerable to two goroutines writing to it at the same time.
+// Go can help us to spot race conditions with its built in race detector (https://blog.golang.org/race-detector).
+// To enable this feature, run the tests with the race flag: go test -race.
 func TestCheckWebsite(t *testing.T) {
 	websites := []string{
 		"https://genilogi.id",
